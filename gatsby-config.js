3a03ac86faed2f7e61e7ca17341e63512736507b
p0lycarpio <arsene.reymond@free.fr>
2022-03-28 16:51:09 +0200

    remove default config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,12 @@ module.exports = {
     title: `Transatlantic Cultures`,
     siteUrl: `https://transatlantic-cultures.org/`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp",
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+      "name": "articles",
+      "path": "./src/articles/"
     },
     __key: "pages"
   }]
