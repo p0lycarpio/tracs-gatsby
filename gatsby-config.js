8abe443dd292f35e66f808d075aedce6988c66e9
p0lycarpio <arsene.reymond@free.fr>
2022-03-30 12:05:20 +0200

    update config - create Prettier conf - fix backticks

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,23 @@
 module.exports = {
   siteMetadata: {
     title: `Transatlantic Cultures`,
-    siteUrl: `https://transatlantic-cultures.org/`
+    siteUrl: `https://transatlantic-cultures.org/`,
+    description: `Programme de recherche international sur la culture`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp",
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "articles",
-      "path": "./src/articles/"
+  
+  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-image`, `gatsby-plugin-mdx`, `gatsby-plugin-sharp`, `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `articles`,
+        path: `${__dirname}/content/articles/`
+      },
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `themes`,
+        path: `${__dirname}/content/themes/`,
+      }
+    }]
 };
---
+++ gatsby-node.js
@@ -0,0 +1,16 @@
+exports.createPages = ({ actions: { createPage } }) => {
+    const themes = require('./content/themes/themes.json')
+
+    themes.forEach(theme => {
+
+        createPage({
+            path: `/themes/${theme.slug}/`,
+            component: require.resolve("./src/pages/themes/theme.js"),
+            context: {
+                title: theme.title,
+                description: theme.description,
+                image: theme.image
+            }
+        })
+    })
+}
