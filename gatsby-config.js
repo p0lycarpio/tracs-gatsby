f63d2ee793bb4160288ab981f00e9393c9418108
p0lycarpio <arsene.reymond@free.fr>
2022-04-11 14:56:29 +0200

    implement search with FlexSearch - create 2 indexes

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,5 +44,76 @@ module.exports = {
         },
       },
     },
+    {
+      // https://github.com/angeloashmore/gatsby-plugin-local-search
+      resolve: `gatsby-plugin-local-search`,
+      options: {
+        name: "articles",
+        engine: "flexsearch",
+        query: `
+          {
+            allMdx(filter: { fileAbsolutePath: { regex: "/articles/" }}) {
+              nodes {
+                id
+                frontmatter {
+                  title
+                  author
+                  date
+                  slug
+                  lang
+                }
+              }
+            }
+          }
+        `,
+        ref: "id", // Unique id
+        index: ["title", "author", "theme_id"], // Fields to index
+        store: ["title", "id", "lang", "slug", "author"],
+        // map the result from the GraphQL query
+        normalizer: ({ data }) =>
+          data.allMdx.nodes.map(node => ({
+            id: node.id,
+            slug: node.frontmatter.slug,
+            title: node.frontmatter.title,
+            author: node.frontmatter.author,
+            lang: node.frontmatter.lang,
+          })),
+      },
+      engineOptions: "performance", // options
+    },
+    {
+      // https://github.com/angeloashmore/gatsby-plugin-local-search
+      resolve: `gatsby-plugin-local-search`,
+      options: {
+        name: "themes",
+        engine: "flexsearch",
+        query: `
+          {
+            allMdx(filter: { fileAbsolutePath: { regex: "/themes/" }}) {
+              nodes {
+                id
+                frontmatter {
+                  title
+                  description
+                  slug
+                }
+              }
+            }
+          }
+        `,
+        ref: "id", // Unique id
+        index: ["title", "description", "theme_id"], // Fields to index
+        store: ["title", "id", "slug", "description"],
+        // map the result from the GraphQL query
+        normalizer: ({ data }) =>
+          data.allMdx.nodes.map(node => ({
+            id: node.id,
+            slug: node.frontmatter.slug,
+            title: node.frontmatter.title,
+            description: node.frontmatter.description,
+          })),
+      },
+      engineOptions: "default", // options
+    },
   ],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,14 @@ import * as React from "react"
 import { heading, flex } from "./layout.module.scss"
 import Nav from "./nav/nav.js"
 import Lang from "./lang"
+import SearchBar from "./search"
 
 const Header = ({ article, site, page }) => {
   return (
     <div>
       <div className={flex}>
         <span className={heading}>{site}</span>
+        <SearchBar />
         <span>{article ? <Lang slug={article}></Lang> : <Lang page={page}></Lang>}</span>
       </div>
       <Nav />
